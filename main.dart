060db679c521123021525feba777b5bbceefc60a
Apurv  Malani <123534851+ApurvMalani@users.noreply.github.com>
2023-07-28 16:40:51 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'Responsive_Onboarding/responsive.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      theme: ThemeData(primaryColor: Colors.purple),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      home: const Responsive(),
+      home: Responsive(),
     );
   }
 }
-
