1b9d8754ac034c3715f7c88eaab277fc66b51df1
Apurv  Malani <123534851+ApurvMalani@users.noreply.github.com>
2023-07-30 14:14:56 +0530

    Update responsive2.dart

--- Responsive_Onboarding/responsive2.dart
+++ Responsive_Onboarding/responsive2.dart
@@ -7,88 +7,66 @@ class Responsivetwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+        body: SafeArea(
+            child: Stack(
+      children: <Widget>[
+        LayoutBuilder(builder: (context, constraints) {
+          double screenwidth = MediaQuery.of(context).size.width;
+          double screenheight = MediaQuery.of(context).size.height;
 
-      body: SafeArea(
-          child: Stack(
-
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 70.0),
+                  child: Center(
 
-              LayoutBuilder(builder: (context , constraints){
+                    child: Image.asset(
+                      Onboarding_Images.img2,
+                      width: screenwidth * 0.8,
+                      height: screenheight * 0.8,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
 
-                double screenwidth = MediaQuery.of(context).size.width;
-                double screenheight = MediaQuery.of(context).size.height;
-
-                return  Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-
-                  children: <Widget>[
-
-                     Expanded(
-                       flex: 1,
-                       child: Padding(
-                         padding: const EdgeInsets.only(top: 60.0),
-                         child: Center(
-                           child: Image.asset(Onboarding_Images.img2,
-                             width: screenwidth * 0.82,
-                             height: screenheight * 0.82,
-                             alignment: Alignment.topCenter,
-
-                           ),
-                         ),
-                       ),
-                     ),
-
-                    Expanded(
-
-                        flex: 1,
-                        child: Column(
-                          children: [
-                            Text('Design Template uploads\n'
-                                '             Every Tuesday!',
-
-                               style: GoogleFonts.poppins(
-                                   fontSize: screenwidth * 0.060,
-                                   fontWeight: FontWeight.w700
-                               ),
-                            ),
-
-                            Center(
-                              heightFactor: 1.8,
-                              child: Text(
-                                'Make sure to take a look my uplab\n'
-                                    '              profile every tuesday',
-                                maxLines: 2,
-                                style: GoogleFonts.lato(
-                                    height: 1.5,
-                                    color: const Color(0xff747474),
-                                    fontSize: screenwidth * 0.045),
-                              ),
-                            )
-
-
-                          ],
-                        )
-
-
-                    )
-
-                  ],
-                );
-
-
-
-
-              })
-
-
+                height: screenheight * 0.03,
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Design Template uploads\n'
+                        '             Every Tuesday!',
+                        style: GoogleFonts.poppins(
+                            fontSize: screenwidth * 0.060,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Center(
+                        heightFactor: 1.8,
+                        child: Text(
+                          'Make sure to take a look my uplab\n'
+                          '              profile every tuesday',
+                          maxLines: 2,
+                          style: GoogleFonts.lato(
+                              height: 1.5,
+                              color: const Color(0xff747474),
+                              fontSize: screenwidth * 0.045),
+                        ),
+                      )
+                    ],
+                  ))
             ],
-
-          )
-
-      )
-
-
-    );
+          );
+        })
+      ],
+    )));
   }
 }
+
